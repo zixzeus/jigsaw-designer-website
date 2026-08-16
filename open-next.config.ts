9ec60e638adcc3f9647b1554dccbf16e73477df2
zixzeus <5369214+zixzeus@user.noreply.gitee.com>
2026-08-16 15:51:26 +0800

    Serve prerendered pages from Workers assets

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,8 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import {defineCloudflareConfig} from "@opennextjs/cloudflare";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
 export default defineCloudflareConfig({
-  // Uncomment to enable R2 cache,
-  // It should be imported as:
-  // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
-  // See https://opennext.js.org/cloudflare/caching for more details
-  // incrementalCache: r2IncrementalCache,
+  // Every published route is prerendered. Bundle those entries with Workers
+  // static assets so deployments do not depend on a mutable R2 cache.
+  incrementalCache: staticAssetsIncrementalCache,
 });
